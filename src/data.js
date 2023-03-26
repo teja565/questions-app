3def624979c009f4a52cddd0ef8e749bac92546c
Krishna Teja <krishnateja565@gmail.com>
2023-03-26 17:20:16 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const questions = [
+  {
+    id: 1,
+    title: "Do I have to allow the use of cookies?",
+    info: "Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.",
+  },
+  {
+    id: 2,
+    title: "How do I change my My Page password?",
+    info: "Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.",
+  },
+  {
+    id: 3,
+    title: "What is BankID?",
+    info: "Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.",
+  },
+  {
+    id: 4,
+    title: "Whose birth number can I use?",
+    info: "Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.",
+  },
+  {
+    id: 5,
+    title: "When do I recieve a password ordered by letter?",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quia voluptatem cum aliquam, repudiandae deserunt corporis laborum expedita rem tempore?",
+  },
+];
+export default questions;
